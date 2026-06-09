0359f633337370793fbb5180a762b4313dac40e9
JCPrime01 <vanzokiller@gmail.com>
2026-06-09 09:25:16 -0300

    chore: shareCode W6INBS94O13 -> H8GCEPIDCPM (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=W6INBS94O13&afp=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=H8GCEPIDCPM&afp=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
